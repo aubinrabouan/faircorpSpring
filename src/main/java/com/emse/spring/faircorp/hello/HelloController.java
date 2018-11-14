115f4956c27944f79038ff55e4b55e6b482941a0
Aubin RABOUAN <aubin.rabouan@etu.emse.fr>
2018-11-14 14:34:19 +0100

    tp REST première partie

---
+++ src/main/java/com/emse/spring/faircorp/hello/HelloController.java
@@ -0,0 +1,4 @@
+package com.emse.spring.faircorp.hello;
+
+public class HelloController {
+}
--- src/test/java/com/emse/spring/faircorp/model/RoomCustomDaoImplTest.java
+++ src/test/java/com/emse/spring/faircorp/model/RoomCustomDaoImplTest.java
@@ -1,4 +1,31 @@
 package com.emse.spring.faircorp.model;
 
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ComponentScan
 public class RoomCustomDaoImplTest {
+
+    @Autowired
+    RoomCustomDao roomCustomDao;
+
+    @Test
+    public void shouldFindByName() {
+        assertThat(roomCustomDao.findByName("Room1"))
+                .extracting("id", "name")
+                .containsExactly(tuple(-10L, "Room1"));
+    }
+
 }
+
